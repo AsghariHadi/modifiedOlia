6a5262ef510ab29bfe281ce5d9911b16aa00bac4
Hadi Asghari <hadi.asgharime@gmail.com>
2023-06-19 17:28:16 +0200

    Update mptcp_olia.c

--- mptcp_olia.c
+++ mptcp_olia.c
@@ -241,7 +241,7 @@ static void mptcp_olia_cong_avoid(struct sock *sk, u32 ack, u32 acked)
 	struct mptcp_olia *ca = inet_csk_ca(sk);
 	const struct mptcp_cb *mpcb = tp->mpcb;
 	//printk(KERN_INFO"Max Window %d",tp->max_window);
-	printk(KERN_INFO"Total packet loss %d",tp->loss);
+	printk(KERN_INFO"Total packet loss %d",tp->lost);
 	//printk(KERN_INFO"Bytes acked %lld",tp->bytes_acked);
 	//printk(KERN_INFO"in flight %d",tp->packets_out);
 	//printk("Generated streamID: %u rtt is (Average of RTTs): %d Reordering: %d Current receiver window is:  %d Current snd_cwnd is: %d the vwnd before loss is %d",streamID, tp->srtt_us,tp->reord_seen, tp->rcv_wnd ,tp->snd_cwnd, tp->prior_cwnd );
